35bcc210452f12d487fe75f6b5530ff8e5d06485
Kevin Dai <daikevin19@yahoo.com>
2019-04-27 07:32:41 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,9 +1,11 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  counter = 0
   loop do
     phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
+    
 end
 
 def times_iterator(number_of_times)
