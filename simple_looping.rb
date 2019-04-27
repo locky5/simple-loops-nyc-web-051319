d36f52b183fbe36d2cda9d2265c2945af5722c34
Kevin Dai <daikevin19@yahoo.com>
2019-04-27 07:33:17 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -6,7 +6,8 @@ def loop_iterator(number_of_times)
     counter += 1
     phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
-    if counter 
+    if counter >= 7
+      break
 end
 
 def times_iterator(number_of_times)
